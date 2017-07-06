7610cab49b7783c578b83efbe1c07fee5a9593ba
tkitamuran <tkitamuran.mail@gmail.com>
2017-07-06 15:37:19 -0700

    メンテナンス

---
+++ public/app.js
@@ -0,0 +1,45 @@
+'use strict';
+var learnjs = {};
+
+learnjs.problems = [
+  {
+      description: "What is truth?",
+      code: "function problem() { return __; }"
+  },
+  {
+    description: "Simple Math",
+    code: "function problem() { return 42 === 6 * __; }"
+  }
+];
+
+learnjs.problemView = function(data) {
+  var problemNumber = parseInt(data, 10);
+  var view = $('.templates .problem-view').clone();
+  view.find('.title').text('Problem #' + problemNumber);
+  learnjs.applyObject(learnjs.problems[problemNumber - 1], view);
+  return view;
+};
+
+learnjs.showView = function(hash) {
+  var routes = {
+    '#problem' : learnjs.problemView
+  };
+  var hashParts = hash.split('-');
+  var viewFn = routes[hashParts[0]];
+  if (viewFn) {
+    $('.view-container').empty().append(viewFn(hashParts[1]));
+  }
+};
+
+learnjs.applyObject = function(obj, elem) {
+  for ( var key in obj ) {
+    elem.find('[data-name="' + key + '"]').text(obj[key]);
+  }
+};
+
+learnjs.appOnReady = function() {
+  window.onhashchange = function() {
+    learnjs.showView(window.location.hash);
+  };
+  learnjs.showView(window.location.hash);
+};
